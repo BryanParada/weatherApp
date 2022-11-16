470ad7c03b490e47e4c5b786660d7031551f4bed
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-15 21:11:09 -0300

    N69 - Model searches

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 import {inquirerMenu,
         readInput,
         pause } from './helpers/inquirer.js';
+import { Searches } from './models/searches.js';
 
 const main = async() =>{
     
+    const searches = new Searches();
+
     let opt;
     
     do{ 
       opt = await inquirerMenu();  
-      console.log(opt);
       
+      switch(opt) {
+
+        case 1:
+          // mostrar mensaje
+          const place = await readInput('City: ');
+console.log(place);
+
+          // buscar los lugares
+
+          //seleccionar el lugar
+
+          //clima
+
+          // mostrar resultados
+          console.log('\nInfo from the city\n'.green);
+          console.log('City:', );
+          console.log('Lat:', );
+          console.log('Lng:', );
+          console.log('Temperature:', );
+          console.log('Minimum:', );
+          console.log('Maximum:', );
+          
+          
+          
+
+          break;
+      }
+
+
  
     if ( opt !== 0) await pause();
 
---
+++ models/searches.js
@@ -0,0 +1,20 @@
+class Searches {
+
+    history = ['Santiago', 'Madrid','San José'];
+
+    constructor() {
+        //TODO: leer db si existe
+    }
+
+    async city( place = '' ){
+        // peticion  http
+        console.log(place);
+
+        //retorna los lugares que coincidan con el place ingresado
+        return []; 
+        
+    }
+
+}
+
+export {Searches}
