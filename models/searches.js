518b34e56d7c4ca679c7e6d69622e5f709774cb1
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-16 10:27:38 -0300

    N74 - Axios instance

--- models/searches.js
+++ models/searches.js
@@ -9,12 +9,29 @@ class Searches {
         //TODO: leer db si existe
     }
 
+    get paramsMapbox(){
+        return{
+            'language': 'en-es', //en%2Ces
+            'access_token': 'pk.eyJ1IjoiYnJ5YW4tcGMiLCJhIjoiY2xhNzRrc3NvMDJ6djQxb3ozNW8zcnpwayJ9.-5db4LGACiAHDDwIWOwVxw' 
+        }
+
+
+    }
+
     async city( place = '' ){
         // peticion  http
         //console.log('City!: ', place);
-
+  
         try {
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
+
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ place }.json`,
+                params: this.paramsMapbox
+            });
+ 
+            const resp = await instance.get();
+
+            //const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/santiago.json?language=en%2Ces&access_token=pk.eyJ1IjoiYnJ5YW4tcGMiLCJhIjoiY2xhNzRrc3NvMDJ6djQxb3ozNW8zcnpwayJ9.-5db4LGACiAHDDwIWOwVxw')
             console.log(resp.data);
 
             return []
